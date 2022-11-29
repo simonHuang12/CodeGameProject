9d18e30dbe0ac9406968e2960f86a613d3fb8e9d
simonHuang12 <simonh8@nycstudents.net>
2022-11-28 19:43:10 -0500

    part 1 generator

--- src/Generator.java
+++ src/Generator.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Generator {
+public class Generator {
+    private int length;
+    private String code;
+
+    public Generator(){ //default values
+        length = 8;
+        code = "";
+    }
+    public Generator(int length){ //custom length
+        this.length = length;
+        code = "";
+    }
+
+    public String generate1Char(){ //generates a single character for the code
+        int random = (int)(Math.random()*94+33);
+        char char1 = (char)random;
+        return ""+char1;
+    }
+    public void generateCode(){ //generates the entire code
+        for (int i = 1; i <= length; i++){
+            code += generate1Char();
+        }
+        System.out.println(code);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for (int i = 0; i < 100; i++){
+            Generator gen = new Generator();
+            gen.generateCode();
+        }
+        for (int i = 33; i < 127; i++){
+            char char1 = (char)i;
+            System.out.print(char1);
+        }
+
     }
 }
